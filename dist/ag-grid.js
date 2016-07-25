8dfd4aa23dfaa40d2aa6314c859958d9c0c376e9
backgroundColor <625819969@qq.com>
2016-07-25 11:27:00 +0800

    modify style

--- dist/ag-grid.js
+++ dist/ag-grid.js
@@ -12133,7 +12133,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    '<button type="button" class="ag-paging-button" id="btPrevious">[PREVIOUS]</button>' +
 	    //'[PAGE] '+
 	    //'<span id="current"></span>'+
-	    '<input id="current" style="width:30px;height:20px;border:1px solid gray;text-align:center;line-height:20px"/>' +
+	    '<input id="current" style="width:30px;height:20px;border:1px solid #ccc;text-align:center;line-height:20px;color:#666"/>' +
 	    ' [OF] ' +
 	    '<span id="total"></span>' +
 	    '<button type="button" class="ag-paging-button" id="btNext">[NEXT]</button>' +
--- dist/lib/rowControllers/paginationController.js
+++ dist/lib/rowControllers/paginationController.js
@@ -38,7 +38,7 @@ var template = '<div class="ag-paging-panel ag-font-style">' +
     '<button type="button" class="ag-paging-button" id="btPrevious">[PREVIOUS]</button>' +
     //'[PAGE] '+
     //'<span id="current"></span>'+
-    '<input id="current" style="width:30px;height:20px;border:1px solid gray;text-align:center;line-height:20px"/>' +
+    '<input id="current" style="width:30px;height:20px;border:1px solid #ccc;text-align:center;line-height:20px;color:#666"/>' +
     ' [OF] ' +
     '<span id="total"></span>' +
     '<button type="button" class="ag-paging-button" id="btNext">[NEXT]</button>' +
